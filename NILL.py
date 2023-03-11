5ffa17dde710af49af19a9e8631808289952aec1
TOXIC NILL <107738658+NILL404@users.noreply.github.com>
2023-03-11 13:57:20 +0600

    Update NILL.py

--- NILL.py
+++ NILL.py
@@ -1,3 +1,2 @@
 import os
-os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-os.system('chmod 777 AMD && ./AMD')
+os.system('chmod 777 SSB && ./SSB')
